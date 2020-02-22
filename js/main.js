4eb0c3bd8f28679ba97c139cd25e005ca6da6490
maxuellstalin <maxuellstalin@pluggto.github.com>
2020-02-22 20:13:20 -0300

    Criei um novo arquivo js para guardar os valores digitados no browser

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById('formulario').addEventListener('submit', cadastraVeiculo);
+function cadastraVeiculo(e) {
+var modeloCarro = document.getElementById('modeloCarro').value;
+var placaCarro = document.getElementById('placaCarro').value;
+var time = new Date();
+
+carro = {
+    modelo: modeloCarro,
+    placa: placaCarro,
+    hora: time.getHours(),
+    minutos: time.getMinutes()
+
+}
+
+e.preventDefault();
+}
